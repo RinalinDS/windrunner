242385a836f81827d6224b1c6f0f638894ccaaa2
DenisPilyutin <denispilyutin@gmail.com>
2024-03-14 01:40:28 +0200

    feat: weather request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+'use client'
 import NavBar from "@/components/NavBar";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>({queryKey: ['weatherdata'], queryFn: async () => {
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`)
+      return data
+    }
+  }
+);
+console.log(data);
+
+if(isLoading) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="animate-bounce">Loading...</p>
+    </div>
+  )
+}
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <NavBar />
---
+++ src/types/weatherType.ts
@@ -0,0 +1,58 @@
+interface WeatherData {
+    cod: string;
+    message: number;
+    cnt: number;
+    list: Forecast[];
+    city: City;
+  }
+  
+  interface Forecast {
+    dt: number;
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+    };
+    weather: Weather[];
+    clouds: {
+      all: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+    };
+    visibility: number;
+    pop: number;
+    sys: {
+      pod: string;
+    };
+    dt_txt: string;
+  }
+  
+  interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }
+  
+  interface City {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  }
