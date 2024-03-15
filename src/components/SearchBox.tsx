6c085b8cea67121853ba72f519b13cee3bd3f06b
DenisPilyutin <denispilyutin@gmail.com>
2024-03-16 01:43:02 +0200

    fix: disable rules and styles

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -20,7 +20,7 @@ export default function SearchBox({className, searchValue, onChange, onSubmit}: 
         placeholder='Search location...' 
         className='px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full'
         />
-        <button className='px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full' disabled={!searchValue}>
+        <button className='px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none enabled:hover:bg-blue-600 h-full disabled:opacity-50' disabled={searchValue.length < 3}>
         <IoSearch />
         </button>
         </form>
