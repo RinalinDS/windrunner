6d36a17cabed15cf2c3ead4ac624d4a6f4b28d1e
DenisPilyutin <denispilyutin@gmail.com>
2024-03-16 20:42:14 +0200

    feat: add util functions

---
+++ src/utils/convertWindSpeed.ts
@@ -0,0 +1,4 @@
+export const convertWindSpeed = (mps: number): string => {
+    const kph = mps * 3.6;
+    return kph.toFixed(0) + 'km/h';
+}
---
+++ src/utils/mToKm.ts
@@ -0,0 +1,4 @@
+export const metersToKilometers =(meters: number): string => {
+    const kilometers = meters / 1000;
+    return kilometers.toFixed(0) + 'km';
+}
