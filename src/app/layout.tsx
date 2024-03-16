adfd2353d73a7bc1960fc1b7cfec3b458758ffb5
DenisPilyutin <denispilyutin@gmail.com>
2024-03-16 20:42:23 +0200

    chore: clear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 'use client'
-
-
 // import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {
   QueryClient,
   QueryClientProvider,
-  useQuery,
 } from '@tanstack/react-query'
 
 const inter = Inter({ subsets: ["latin"] });
