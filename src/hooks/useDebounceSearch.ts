3791741fde171103a0b74f81eb9e37794e1db8ad
DenisPilyutin <denispilyutin@gmail.com>
2024-03-19 01:56:54 +0200

    feat: add useDebounseSearch

---
+++ src/hooks/useDebounceSearch.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+export function useDebounceSearch<T>(value: T, delay = 100): T {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+  
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [value, delay]);
+  
+    return debouncedValue;
+  }
