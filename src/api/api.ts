7e01ab6e3418a15ef911dd23a473700c6582624b
DenisPilyutin <denispilyutin@gmail.com>
2024-03-19 01:55:14 +0200

    feat: api and instance

---
+++ src/api/api.ts
@@ -0,0 +1,26 @@
+import { SuggestionWeatherData } from "@/types/suggestionType";
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: 'https://api.openweathermap.org/data/2.5/',
+});
+
+type Params = {
+    latitude: number
+    longitude: number
+}
+
+export const WeatherApi = {
+    getUniqueSuggestions: async (debouncedSearchValue: string) => {
+        const response = await instance.get<SuggestionWeatherData>(`find?q=${debouncedSearchValue}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
+        const suggestions = response.data.list.map((item) => item.name)
+        const uniqueSuggestion = [...new Set(suggestions)]
+        
+        return uniqueSuggestion
+    },
+
+    getCityByCoords: async ({latitude, longitude}: Params) => {
+        const response = await instance.get(`weather?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
+        return response
+    },
+}  
--- src/api/queries/useGetForecast.ts
+++ src/api/queries/useGetForecast.ts
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import { instance } from "../api"
 
 type Params = {
     currentCity: string
 }
 
 const getForecast = async ({ currentCity }: Params) => {
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=40&units=metric`)
+    const { data } = await instance.get(`forecast?q=${currentCity}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=40&units=metric`)
     return data
 }
 
