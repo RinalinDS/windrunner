709c5fce8a1948bd354deddf6cd46f666dde5216
DenisPilyutin <denispilyutin@gmail.com>
2024-03-21 00:17:26 +0200

    feat: extend loader test

--- src/components/__tests__/Loader.spec.tsx
+++ src/components/__tests__/Loader.spec.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { logRoles, render, screen } from '@testing-library/react';
 import Loader from '../Loader';
 import { expect, describe, it } from 'vitest';
 
 describe('Loader should be rendered', () => {
-  it('should return 11km with args 11234', () => {
+  it('Loader should be rendered', () => {
     render(<Loader />);
     const loadingString = screen.getByText('Loading...');
 
     expect(loadingString).toBeInTheDocument();
+  });
+  it('should be rendered insider container', () => {
+    const { container } = render(<Loader />);
+    expect(container.firstChild).toHaveClass(
+      'min-h-screen flex items-center justify-center'
+    );
+    expect(container.firstChild?.firstChild).toHaveClass('animate-bounce');
   });
 });
