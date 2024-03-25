81f10ff6fd2a53c40b0ace466404e429115c6768
DenisPilyutin <denispilyutin@gmail.com>
2024-03-26 01:33:52 +0200

    feat: cn util testing

---
+++ src/utils/__tests__/cn.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { cn } from '../cn';
+
+describe('cn util function should return correct string', () => {
+  it('should return default', () => {
+    expect(cn('relative h-20 w-20')).toBe('relative h-20 w-20');
+  });
+
+  it('should return correctly ignore undefined', () => {
+    expect(cn('relative h-20 w-20', undefined)).toBe('relative h-20 w-20');
+  });
+
+  it('should return correctly concat arguments', () => {
+    expect(cn('relative ', 'h-20 w-20')).toBe('relative h-20 w-20');
+  });
+});
