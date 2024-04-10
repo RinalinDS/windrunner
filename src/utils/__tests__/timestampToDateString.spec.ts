21d89299a4fa464af8e1d0d579f7665927f629b4
DenisPilyutin <denispilyutin@gmail.com>
2024-04-10 15:35:00 +0300

    feat: add tests for timestamptodatestring

---
+++ src/utils/__tests__/timestampToDateString.spec.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect, test } from 'vitest';
+import { timestampToDateString } from '../timestampToDateString';
+
+describe('timestampToDateString', () => {
+  it('should return the correct date for a given timestamp', () => {
+    const timestamp = 1618085994;
+    const expectedDate = '2021-04-10';
+
+    const result = timestampToDateString(timestamp);
+
+    expect(result).toBe(expectedDate);
+  });
+
+  it("should return today's date for the current timestamp", () => {
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const expectedDate = new Date(currentTimestamp * 1000)
+      .toISOString()
+      .split('T')[0];
+
+    const result = timestampToDateString(currentTimestamp);
+
+    expect(result).toBe(expectedDate);
+  });
+
+  it('should return the correct date for a negative timestamp', () => {
+    const timestamp = -86400;
+    const expectedDate = '1969-12-31';
+
+    const result = timestampToDateString(timestamp);
+
+    expect(result).toBe(expectedDate);
+  });
+});
