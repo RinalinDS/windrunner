5055e3c672a3bc5a7190da239566d7b1d7ad7e62
DenisPilyutin <denispilyutin@gmail.com>
2024-04-13 01:55:27 +0300

    fix: useDebounce testing

--- src/hooks/__tests__/useDebounceSearch.spec.ts
+++ src/hooks/__tests__/useDebounceSearch.spec.ts
@@ -4,9 +4,15 @@ import { expect } from 'vitest';
 import { renderHook } from '@testing-library/react';
 
 describe('useDebounceSearch', () => {
-  it('should return the same value when delay is not exceeded', () => {
-    const value = 'test';
-    const { result } = renderHook(() => useDebounceSearch(value, 100));
-    expect(result.current).toBe(value);
+  it('should return the same value when delay is not exceeded', async () => {
+    const { result } = renderHook(() => useDebounceSearch('test', 100));
+
+    expect(result.current).toBe('test');
+
+    result.current = 'new value';
+
+    await new Promise((resolve) => setTimeout(resolve, 150));
+
+    expect(result.current).toBe('new value');
   });
 });
