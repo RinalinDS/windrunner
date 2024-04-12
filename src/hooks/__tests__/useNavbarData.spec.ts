b1423548f0ec7b8651b6f8c7cb38f39c4ce4859d
DenisPilyutin <denispilyutin@gmail.com>
2024-04-13 01:46:23 +0300

    feat: add more test cases for useNavbar

--- src/hooks/__tests__/useNavbarData.spec.ts
+++ src/hooks/__tests__/useNavbarData.spec.ts
@@ -1,8 +1,9 @@
 import { WeatherApi } from '@/api/api';
 import { renderHook } from '@testing-library/react';
-import { afterAll, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { useNavBarData } from '../useNavbarData';
-import { useDebounceSearch } from '../useDebounceSearch';
+import { act } from 'react-dom/test-utils';
+import { ChangeEvent } from 'react';
 
 describe('useNavbarData', () => {
   const setCurrentCity = vi.fn();
@@ -34,5 +35,42 @@ describe('useNavbarData', () => {
     expect(getUniqueSuggestionsSpy).toHaveBeenCalledTimes(1);
 
     getUniqueSuggestionsSpy.mockRestore();
+    getUniqueSuggestionsSpy.mockClear();
+  });
+
+  it('onChange will cause searchValue to update and request to be called', () => {
+    const { result } = renderHook(() => useNavBarData(setCurrentCity));
+
+    const e = {
+      currentTarget: {
+        value: 'hello',
+      },
+    };
+    act(() => result.current.onChange(e as ChangeEvent<HTMLInputElement>));
+
+    expect(result.current.searchValue).toEqual('hello');
+
+    e.currentTarget.value = 'goodbye';
+
+    act(() => result.current.onChange(e as ChangeEvent<HTMLInputElement>));
+
+    expect(result.current.searchValue).toEqual('goodbye');
+  });
+
+  it('should render the search input field and suggestions list', () => {
+    const setCurrentCity = vi.fn();
+
+    const { result } = renderHook(() => useNavBarData(setCurrentCity));
+
+    expect(result.current.searchValue).toBe('');
+    expect(result.current.error).toBe('');
+    expect(result.current.suggestions).toEqual([]);
+    expect(result.current.showSuggestions).toBe(false);
+
+    act(() => {
+      result.current.handleSuggestionClick('New York');
+    });
+
+    expect(setCurrentCity).toHaveBeenCalledWith('New York');
   });
 });
